495bba6aa39286cf0d8e5b6241746dc73f62bd36
KlcAhmet <ahmetbatukilic@gmail.com>
2022-02-14 22:31:54 +0300

    update geo-names.js

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,16 @@
 import { createStore } from 'vuex'
+import GeoNames from '@/store/modules/geo-names'
 
 export default createStore({
-  state: {},
-  mutations: {},
+  state: {
+    search_loading: false,
+  },
+  mutations: {
+    SET_SEARCH_LOADING: (state) =>
+      (state.search_loading = !state.search_loading),
+  },
   actions: {},
-  modules: {},
+  modules: {
+    GeoNames,
+  },
 })
