6ab1d99fbd7d79ffc16a09ed9833c55448b28f2e
KlcAhmet <ahmetbatukilic@gmail.com>
2022-02-14 21:20:31 +0300

    install axios and append store

--- src/main.js
+++ src/main.js
@@ -2,5 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import * as plugins from '@/plugins'
 
-createApp(App).use(store).use(router).mount('#app')
+store.$axios = plugins.axios
+store.$env = process.env
+
+const app = createApp(App)
+
+app.use(store).use(router).mount('#app')
