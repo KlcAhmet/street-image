1c821be9668777ab888be5d0b4ec606b1b376730
KlcAhmet <ahmetbatukilic@gmail.com>
2022-02-16 22:26:22 +0300

    add lazy loading

--- src/components/image list/ImageCard.vue
+++ src/components/image list/ImageCard.vue
@@ -2,19 +2,39 @@
 .w-full.py-2
   .flex.flex-col
     img(
+      v-show="show"
       class="w-full h-48"
       :src="imageSrc"
+      @load="imageloaded"
+      )
+    img(
+      v-show="!show"
+      class="w-full h-48"
+      :src="LoadingImage"
+      @load="imageloaded"
       )
     .text-center.font-medium(class="pt-5") {{ name }}
 </template>
 
 <script>
+import LoadingImage from '@/assets/images/loading-image.png'
 export default {
   props: {
     name: String,
     imageSrc: String,
   },
   name: 'ImageCard',
+  data() {
+    return {
+      LoadingImage,
+      show: false,
+    }
+  },
+  methods: {
+    imageloaded() {
+      this.show = true
+    },
+  },
 }
 </script>
 
