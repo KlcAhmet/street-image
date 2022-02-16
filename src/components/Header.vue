6d05dda497dc77044e65c18de29dca0b435bb918
KlcAhmet <ahmetbatukilic@gmail.com>
2022-02-16 06:56:21 +0300

    header component made responsive

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,11 @@
 <template lang="pug">
 .header.py-12
-  .grid.grid-cols-2
-    .flex.flex-row.justify-end.flex-wrap
+  .flex.flex-wrap(
+    class="flex-col-reverse justify-center md:justify-end md:flex-row"
+    )
+    .flex.flex-row.justify-center(class="w-11/12 md:w-8/12 lg:w-6/12 ")
       input.shadow-lg.shadow-black.rounded-l-md.px-5(
-        class="w-9/12"
+        class="w-8/12"
         type="text"
         placeholder="Location"
         )
@@ -11,7 +13,7 @@
         type="button"
         )
        | Search
-    .flex.justify-end
+    .flex.justify-end(class="w-11/12 md:w-4/12 lg:w-3/12 pb-10 md:pb-0 sm:pr-10 md:pr-0")
       img(:src="HeaderLogo")
 </template>
 
