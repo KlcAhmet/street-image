a638f6cea78b0af151de763ff8e16e50a1aa864e
KlcAhmet <ahmetbatukilic@gmail.com>
2022-02-16 04:09:36 +0300

    update store root

--- src/store/index.js
+++ src/store/index.js
@@ -2,8 +2,12 @@ import { createStore } from 'vuex'
 import Search from '@/store/modules/search'
 
 export default createStore({
-  state: {},
-  mutations: {},
+  state: {
+    selectedImages: null,
+  },
+  mutations: {
+    SET_SELECTED_IMAGES: (state, payload) => (state.selectedImages = payload),
+  },
   actions: {},
   modules: {
     Search,
